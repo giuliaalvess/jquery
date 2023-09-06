f9a2491a31b4984fdd993b29629d49e2f6fd4e9b
giulia <134700744+giuliaalvess@users.noreply.github.com>
2023-09-05 21:19:53 -0300

    prontooo

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $(".btn-success").click(function(){
+        alert("Nossos últimos conteúdos foram sobre sustentabilidade e pegada ecológica em biologia, interação molecular em quimica, e em física função horaria da Posição do MRU ");
+    });
+
+    $(".btn-danger").click(function(){
+        alert("Em ciências humanas estamos aprendendo sobre leis de terras, capitanias hereditárias e posseiros X grileiros");
+      });
+
+      $(".btn-primary").click(function(){
+        alert("Em matemática estamos aprendendo sobre logaritmo de um número");
+      });
+
+      $(".btn-info").click(function(){
+        alert("Em linguages estamos aprendendo sobre curta-metragem, literatura(gênero dramático e seus subgêneros) e sobre future tenses");
+      });
+
+      $(".btn-warning").click(function(){
+        alert("Em técnico aprendemos a utilizar o HTML, CSS e JS no Bootstrap");
+      });
+      
+      $(".btn-dark").click(function(){
+        alert("Em PP estamos aprendendo sobre empresas, MEI e direitos humanos ");
+      });
+
+      $(".btn-secondary").click(function(){
+        alert("Em PV estamos apresentando o projeto de miniaulas");
+      });
+});
